4c14dcc088734f3fa6cd91a8abbf55d847566d2f
Favour Ojo <ojof@allegheny.edu>
2021-12-06 22:32:08 -0500

    fix

--- src/analysis.r
+++ src/analysis.r
@@ -8,13 +8,25 @@
 
 rm(list = ls()) # remove variables stored in memory.
 
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
 # add your libraries here
 
 library(tidyverse)
 
+# If needed, install psych package 
 # install.packages("psych")
 library(psych)
 
+#Selecting the file
+myFile  <- file.choose() # pick the csv filename
+myData <- read.csv(myFile) # load the data
+
+view(myData)
 # add your code here. Be sure to leave your data file(s) in the data/ directory of this repository.
 
 # TODO
